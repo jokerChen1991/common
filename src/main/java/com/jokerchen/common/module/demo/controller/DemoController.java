e8552127621abdea726ab11fbdf1fddd530a6536
jokerChen <chenxj_2010@163.com>
2018-08-06 11:26:24 +0800

    common project commit

---
+++ src/main/java/com/jokerchen/common/module/demo/controller/DemoController.java
@@ -0,0 +1,23 @@
+package com.jokerchen.common.module.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+
+
+@Data
+@Slf4j
+@Controller
+@RequestMapping("/")
+public class DemoController {
+
+	
+	@RequestMapping("toIndex")
+	public String toIndex() {
+		log.info("toIndex");
+		return "html/demo/index";
+	}
+}
